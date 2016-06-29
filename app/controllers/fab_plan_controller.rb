ff432ff17f3286961e75be1ddc6f4f428b48f1a2
Dillon Huff <dillonhuff@gmail.com>
2016-06-29 14:22:12 -0700

    Now have text box for gcode working

--- app/controllers/fab_plan_controller.rb
+++ app/controllers/fab_plan_controller.rb
@@ -6,5 +6,8 @@ class FabPlanController < ApplicationController
   end
 
   def download
+    fname = File.join(Rails.root, 'app', 'plans', 'final_plan.json')
+    f = File.read(fname)
+    render plain: f
   end
 end
