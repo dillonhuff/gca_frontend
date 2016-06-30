0c9ba5da3a3eab39243279f3240d1ac3ed462461
Dillon Huff <dillonhuff@gmail.com>
2016-06-29 19:18:30 -0700

    Now have actual json parsing for construction of the plan inputs

--- app/controllers/fab_plan_controller.rb
+++ app/controllers/fab_plan_controller.rb
@@ -11,9 +11,76 @@ class FabPlanController < ApplicationController
     render plain: f
   end
 
+  def tools()
+    tools ||= []
+    Tool.all.each do |t|
+      tools << {:tool_diameter => t.diameter,
+                :tool_length => t.length,
+                :tool_num_flutes => t.numFlutes,
+                :tool_material => t.material}
+    end
+    return tools
+  end
+
+  # TODO: Need to actually allow user workpiece inputs
+  def workpiece()
+    stock = {:x => [3.5, 0, 0],
+             :y => [0, 3.0, 0],
+             :z => [0, 0, 3.0],
+             :workpiece_material => "ACETAL"}
+    return stock
+  end
+
+  # TODO: Actually allow many different vices and pick one
+  def vice()
+    v = Vice.all[0]
+    return {:vice_pos => [v.xPos, v.yPos, v.zPos],
+            :vice_x_length => v.xLength,
+            :vice_y_length => v.yLength,
+            :vice_base_height => v.baseHeight,
+            :vice_top_height => v.topHeight,
+            :vice_clamp_width => v.clampWidth,
+            :vice_max_jaw_width => v.maxJawWidth,
+            :vice_protective_base_plate_height => v.protectiveBasePlateHeight}
+  end
+
+  def base_plates()
+    plates ||= []
+    BasePlate.all.each do |p|
+      plates << p.height
+    end
+    return plates
+  end
+
+  def parallel_plates()
+    plates ||= []
+    ParallelPlate.all.each do |p|
+      plates << p.height
+    end
+    return plates
+  end
+
+  def fixtures()
+    fixes = {:vice => vice(),
+             :base_plates => base_plates(),
+             :parallel_plates => parallel_plates()}
+    return fixes
+  end
+
+  def create_inputs(output_file_name)
+    inputs_json = {:tools => tools(),
+                   :fixtures => fixtures(),
+                   :workpiece => workpiece()}.to_json
+    out_path = File.join(Rails.root, 'app', 'inputs', output_file_name)
+    File.open(out_path, "wb") { |f| f.write(inputs_json) }
+  end
+
   def upload
     name = params[:stl_upload].original_filename
     path = File.join(Rails.root, 'app', 'meshes', name)
+
+    create_inputs('inputs.json');
+    
     @upload_path = path
     File.open(path, "wb") { |f| f.write(params[:stl_upload].read) }
 
